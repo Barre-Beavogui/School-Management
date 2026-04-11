9cf1d69fa108c4d08587bbe5f4fb8ce42630cb84
Barre-Beavogui <Barre-Beavogui@users.noreply.github.com>
2026-04-11 18:09:55 +0200

    Rename branding to Barre et Soul Academy

--- managementProject/settings.py
+++ managementProject/settings.py
@@ -171,7 +171,7 @@ if CLOUDINARY_URL:
 
 
 EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="School Management <no-reply@example.com>")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="Barre et Soul Academy <no-reply@example.com>")
 EMAIL_HOST = config("EMAIL_HOST", default="")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
